1d7ab4007fa0507979892aee88acf068de732bbf
yidane <yidane@163.com>
2018-08-07 15:47:58 +0800

    update http.go

--- http/http.go
+++ http/http.go
@@ -12,24 +12,32 @@ func NewRequest(method, url string, cookies []*http.Cookie) (*http.Request, erro
 		return nil, err
 	}
 
-	req.Header.Add("Host", "rid.nxin.com")
+	setHeaders(req, "rid.nxin.com", "http://rid.nxin.com", "http://rid.nxin.com/login.html")
+	setCookies(req, cookies)
+	return req, nil
+}
+
+func setHeaders(req *http.Request, host, origin, referer string) {
+	req.Header.Add("Host", host)
 	req.Header.Add("Connection", "keep-alive")
-	req.Header.Add("Origin", "http://rid.nxin.com")
+	req.Header.Add("Origin", origin)
 	req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36") // nothing
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")                                                            // must
 	req.Header.Add("Accept", "application/json, text/javascript, */*; q=0.01")
 	req.Header.Add("X-Requested-With", "XMLHttpRequest")
-	req.Header.Add("Referer", "http://rid.nxin.com/login.html")
+	req.Header.Add("Referer", referer)
 	req.Header.Add("Accept-Encoding", "gzip, deflate")
 	req.Header.Add("Accept-Language", "zh-CN,zh;q=0.8")
+}
 
-	if cookies != nil {
-		for i := 0; i < len(cookies); i++ {
-			req.AddCookie(cookies[i])
-		}
+func setCookies(req *http.Request, cookies []*http.Cookie) {
+	if cookies == nil {
+		return
 	}
 
-	return req, nil
+	for i := 0; i < len(cookies); i++ {
+		req.AddCookie(cookies[i])
+	}
 }
 
 func GetResponseContent(req *http.Request) ([]byte, []*http.Cookie, error) {
